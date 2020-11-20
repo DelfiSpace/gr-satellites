6f090341bda0dfa6a44ed5c4fe001cc32171ccf3
Stefano Speretta <s.speretta@tudelft.nl>
2020-11-20 15:29:35 +0100

    Added timestamp and better documentation and error handling

--- python/delfispace_submitter.py
+++ python/delfispace_submitter.py
@@ -9,18 +9,20 @@
 
 # This contains code by Peter Horvath <hp@hvt.bme.hu>
 # (source: private communication)
+# This contains code by Stefano Speretta <s.speretta@tudelft.nl>
 
 from gnuradio import gr
 import pmt
 import satellites
 
+import datetime
 import json
 import requests
 from requests.auth import HTTPBasicAuth
 
 class delfispace_submitter(gr.basic_block):
     """
-    Submits telemetry to https://gnd.bme.hu:8080/
+    Submits telemetry to the Delfi Space Telemetry server
     """
     def __init__(self, user, passphrase, satellite):
         gr.basic_block.__init__(self,
@@ -46,34 +48,35 @@ class delfispace_submitter(gr.basic_block):
             self.auth_token = auth_resp['token'] # the token is valid for 60 minutes, after 45 a new one can be requested
         elif rauth.status_code == 401:
             # unauthorized (wrong credentials)
-            print('Wrong credentials, have you registered at https://gnd.bme.hu:8080/ ?')
+            print('Wrong credentials, have you registered on the Delfi Space telemetry server?')
         else:
             print(f'Authentication failed, error code = {rauth.status_code}')
 
-    def putPacket(self, frame):
+    def putPacket(self, now, frame):
         #if self.auth_token is None:
         #    print('Not uploading packet to BME, as we are not authenticated')
         #    return
-        packets = [{'packet': frame.hex().upper()}]
+        packets = [{'timestamp': str(now), 'packet': frame.hex().upper()}]
         #auth_header = {'Authorization': 'Bearer ' + self.auth_token}
         use_header = {"User-Agent": "gr-satellite/"+satellites.__version__}
 
         rpacket = requests.post(self.url+'submit', json={'packets':packets}, headers=use_header, timeout=10)
 
         #packet_resp = rpacket.json()
-        if rpacket.status_code != 200:
+        if rpacket.status_code != 201:
         #    uploaded_packets = packet_resp["results"]
         #    for p in uploaded_packets:
         #        if 'error' in p:
         #            print('Checksum error')
         #else:
-            print("Packet upload failed, token might have expired!")
+            print("Error " + str(rpacket.status_code) + ": " + rpacket.text)
                 
     def handle_msg(self, msg_pmt):
+        now = datetime.datetime.utcnow()
         msg = pmt.cdr(msg_pmt)
         if not pmt.is_u8vector(msg):
             print("[ERROR] Received invalid message type. Expected u8vector")
             return
 
         frame = bytes(pmt.u8vector_elements(msg))
-        self.putPacket(frame)
+        self.putPacket(now, frame)
